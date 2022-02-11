8d4580eb81832cb98bfd048819780e7484fc4167
Aizkun <maxime.t13m@gmail.com>
2022-02-11 14:50:46 +0100

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# polls-and-surveys
- 
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polls_and_surveys.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polls_and_surveys_.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
